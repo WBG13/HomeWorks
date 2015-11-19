defb196dd376fff6e5a8388f533cd7ee6a6d25f0
WBG13 <DarkStrangerOfChaos@ya.ru>
2015-11-19 17:43:56 +0500

    Create Main.java

---
+++ TicTacToe/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Thread game = new Thread(new Game(true, false));
+        game.start();
+        javafx.application.Application.launch(GUIStageBuilder.class);
+    }
+}
