89bf7c435770ae34a56248784a0771b91c3942ab
WBG13 <DarkStrangerOfChaos@ya.ru>
2015-11-19 19:01:02 +0500

    Delete Main.java

--- TicTacToe/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        Thread game = new Thread(new Game(true, false));
-        game.start();
-        javafx.application.Application.launch(GUIStageBuilder.class);
-    }
-}
