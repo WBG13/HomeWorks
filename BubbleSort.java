c814edb4613ef5e3fb9458687760f9a8631af120
WBG13 <DarkStrangerOfChaos@ya.ru>
2016-02-08 19:56:44 +0500

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,38 +1,36 @@
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] specimens = new int[10];
-        for (int i = 0; i < specimens.length; i++) {
-            specimens[i] = (int) Math.round(Math.random() * 100);
-            System.out.println(specimens[i] + "	");
+        BubbleSort  b = new BubbleSort ();
+        int[] specimens = t.create();
+        b.print(specimens);
+        b.print(b.bubbleSort(specimens));
+}
+
+    public int[] create() {
+        int[] j = new int[10];
+        for (int i = 0; i < j.length; i++) {
+            j[i] = (int) Math.round(Math.random() * 100);
         }
-        Sort1 sort = new Sort1(specimens);
-        specimens = sort.bubbleSort();
-        System.out.println();
-        for (int i = 0; i < specimens.length; i++)
-            System.out.print(specimens[i] + " ");
+        return j;
     }
 
-    static class Sort1 {
-        private int[] specimens;
+    public void print(int input[]) {
+        for (int i : input) System.out.print(i + " ");
+        System.out.println();
+    }
 
-        public Sort1(int[] specimens) {
-            this.specimens = specimens;
-        }
+    private void swap(int[] specimens, int index) {
+        int temp = specimens[index - 1];
+        specimens[index - 1] = specimens[index];
+        specimens[index] = temp;
+    }
 
-        private void swap(int[] specimens, int index) {
-            int temp = specimens[index - 1];
-            specimens[index - 1] = specimens[index];
-            specimens[index] = temp;
-        }
-
-        public int[] bubbleSort() {
-            int[] resSpecimens = specimens;
-            for (int i = 1; i < resSpecimens.length; i++) {
-                for (int j = resSpecimens.length - 1; j >= i; j--) {
-                    if (resSpecimens[j - 1] > resSpecimens[j]) swap(resSpecimens, j);
-                }
+    public int[] bubbleSort(int input[]) {
+        for (int i = 1; i < input.length; i++) {
+            for (int j = input.length - 1; j >= i; j--) {
+                if (input[j - 1] > input[j]) swap(input, j);
             }
-            return resSpecimens;
         }
+        return input;
     }
 }
