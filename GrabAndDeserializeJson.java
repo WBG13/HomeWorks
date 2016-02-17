5e998daac3cd77221f8c573acaa6354bc66066f6
WBG13 <DarkStrangerOfChaos@ya.ru>
2016-02-17 16:34:01 +0500

    Update GrabAndDeserializeJson.java

--- GrabAndDeserializeJson.java
+++ GrabAndDeserializeJson.java
@@ -1,18 +1,13 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class JsonORfU {
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 
-    private static String readAll(Reader rd) throws IOException {
+
+public class ReadJsonFromURL {
+private String readAll(Reader rd) throws IOException {
         StringBuilder sb = new StringBuilder();
         int cp;
         while ((cp = rd.read()) != -1) {
@@ -21,20 +16,21 @@ public class JsonORfU {
         return sb.toString();
     }
 
-    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL("http://jsonplaceholder.typicode.com/posts/1").openStream();
+    public JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream inpStream = new URL(url).openStream();
         try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
+            BufferedReader br = new BufferedReader(new InputStreamReader(inpStream, Charset.forName("UTF-8")));
+            String jsonText = readAll(br);
             JSONObject json = new JSONObject(jsonText);
             return json;
         } finally {
-            is.close();
+            inpStream.close();
         }
     }
 
     public static void main(String[] args) throws IOException, JSONException {
-        JSONObject json = readJsonFromUrl("http://jsonplaceholder.typicode.com/posts/1");
+        ReadJsonFromURL rJS = new ReadJsonFromURL();
+        JSONObject json = rJS.readJsonFromUrl("http://jsonplaceholder.typicode.com/posts/1");
         System.out.println(json.get("id"));
         System.out.println(json.get("userId"));
         System.out.println(json.get("title"));
