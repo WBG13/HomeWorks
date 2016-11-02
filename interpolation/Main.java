ffa6d65f02f63275608faef8a07856d788590055
WBG13 <DarkStrangerOfChaos@ya.ru>
2016-11-02 16:07:28 +0500

    Update Main.java

--- interpolation/Main.java
+++ interpolation/Main.java
@@ -1,14 +1,7 @@
-package LagrangesInterpolation.HW.GUI;
-
-import LagrangesInterpolation.HW.GUI.Controller.Controller;
-
-/**
- * Created by TH-221 on 18.09.2016.
- */
 public class Main {
     public static void main(String[] args) {
         int [] x = {0, 10, 20, 30, 40};
-        int [] y =  {4, 16, 20, 50, 55};
-        Controller cn = new Controller(x ,y);
+        int [] y = {4, 16, 20, 50, 55};
+        new Controller(x ,y);
     }
 }
